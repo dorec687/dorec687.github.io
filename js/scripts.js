408c1398d0a4f8222ca1d05bbbd48f9a4bb0f2f2
aimei89 <86515587+dorec687@users.noreply.github.com>
2021-06-28 21:47:32 +0800

    fixing something

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,13 @@ function initCashemir() {
 		this.removeAttribute("controls");
 		}
 
+	}).on('mouseout', function(event) {
+		if(isMobile == false){
+		this.currentTime = tempVidTime;
+		this.pause();
+		this.removeAttribute("controls");
+		}
+
 	});
 	
 // show menu ------------------	
